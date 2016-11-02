0263c4675be15c8475d605289ea76b4e7714989a
Kevin Diaz <kevrocks67@gmail.com>
2016-11-02 09:49:24 -0400

    Initial commit client side software Visual Basic

---
+++ Computer Cart Management Software/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ComboBox1.Show()
+        ComboBox1.Items.Add("Computer Type")
+        ComboBox1.Items.Add("Room Number")
+        ComboBox1.Items.Add("Cart Number")
+        ComboBox1.Items.Add("Amount of Computers")
+    End Sub
+End Class
