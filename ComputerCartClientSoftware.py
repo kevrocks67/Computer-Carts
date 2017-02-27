77d576b8ef37b61084bf3e0823e97a88758c20d7
Kevin Diaz <kevrocks67@gmail.com>
2017-02-27 14:19:11 -0500

    Began work on top toolbar on Main Screen

--- ComputerCartClientSoftware.py
+++ ComputerCartClientSoftware.py
@@ -3,27 +3,19 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.lang import Builder
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-     ObjectProperty
+from kivy.uix.gridlayout import GridLayout
 #from sql import *
 #from sql.aggregate import *
 #from sql.conditionals import *
 
 class ComputerCartMain(Widget):
-    box = ObjectProperty(None)
-
-class BoxT(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
+    pass
+class ToolbarGridLayout(GridLayout):
+    pass
+         
 class ComputerCartMSApp(App, Widget):
     def build(self):
         Builder.load_file('ComputerCartMS.kv')
-        #return Button(text="hello", background_color=(0, 1, 1, 1), font_size=50)
         main = ComputerCartMain()
         return main
 
