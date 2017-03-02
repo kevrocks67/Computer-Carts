d84a824ecb38ee582503e7089668eb3a57301f5f
Kevin Diaz <kevrocks67@gmail.com>
2017-03-02 16:11:30 -0500

    Changed to using Action Bar for top buttons

--- ComputerCartClientSoftware.py
+++ ComputerCartClientSoftware.py
@@ -4,20 +4,35 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
+from kivy.base import runTouchApp
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 #from sql import *
 #from sql.aggregate import *
 #from sql.conditionals import *
 
-class ComputerCartMain(Widget):
-    pass
-class ToolbarGridLayout(GridLayout):
-    pass
-         
-class ComputerCartMSApp(App, Widget):
-    def build(self):
-        Builder.load_file('ComputerCartMS.kv')
-        main = ComputerCartMain()
-        return main
-
-if __name__ == '__main__':
-    ComputerCartMSApp().run()
+runTouchApp(Builder.load_string('''
+ActionBar:
+    pos_hint: {'top':1}
+    ActionView:
+        use_separator: True
+        ActionPrevious:
+            title: 'Computer Cart Management Software Alpha'
+            with_previous: False
+        ActionOverflow:
+        ActionButton:
+            text: 'Add'
+        ActionButton:
+            text: 'Remove'
+        ActionButton:
+            text: 'Edit'
+        ActionButton:
+            text: 'Unlock'
+        ActionButton:
+            text: 'Lock'
+        ActionGroup:
+            text: 'Group1'
+            ActionButton:
+                text: 'Settings'
+'''))
