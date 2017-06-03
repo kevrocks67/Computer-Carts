3a97641d148283336c7bd17602e929f88ad25d7e
Kevin Diaz <kevrocks67@gmail.com>
2017-06-03 00:06:50 -0400

    Merge branch 'master' of https://github.com/kevrocks67/Computer-Carts Figured out that RecycleView is not screenmanager friendly so I am now using an animated layout with text boxes on the main screen Added Login Screen design but it still needs work and it breaks when the password is entered incorrectly. Also added popup for registering users. Fixed notification dropdown bug Realized the text inputs were missing the type field and added it. This also lead to the realization that the SQL table is missing a lock status column. Made Columbia Secondary logo the icon for the application.

--- Computer Cart Client Software/ComputerCartClientSoftware.py
+++ Computer Cart Client Software/ComputerCartClientSoftware.py
@@ -8,6 +8,7 @@ from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
 from kivy.properties import BooleanProperty
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
@@ -22,15 +23,22 @@ currentObj = 0
 users = []
 salts = []
 hashes = []
+retry = 0
+addState = 1
+editState = 1
 
 
 class LoginScreen(Screen):
-    def register(self):
+    def register_butt(self):
+        r = RegisterPopup()
+        r.open()
+
+    def register(self, user, password):
         global users
         global salts
         global hashes
-        user = input('Enter desired username: ')
-        password = input('Enter desired password: ')
+        # user = input('Enter desired username: ')
+        # password = input('Enter desired password: ')
 
         salt = uuid.uuid4().hex
 
@@ -40,23 +48,26 @@ class LoginScreen(Screen):
         users.append(user)
         salts.append(salt)
         hashes.append(salted_hash)
-        self.check_auth()
 
-    def check_auth(self):
+        print('User: {} was registered'.format(user))
+        # self.check_auth()
+
+    def check_auth(self, user, password):
         global users
         global salts
         global hashes
-        while True:
-            user = input('Enter username: ')
-            if user in users:
-                print('Username exists')
-                index = users.index(user)
-                break
+        global retry
+        # while True:
+        # user = input('Enter username: ')
+        if user in users:
+            print('Username exists')
+            index = users.index(user)
+            # self.ids.inp_username.text = ''
+            self.ids.inp_password.text = ''
+            # break
+            # for retry in range(5):
+            # password = input('Enter password: ')
 
-            print('User does not exist')
-
-        for retry in range(5):
-            password = input('Enter password: ')
             salt = salts[index]
 
             salted_pass = password + salt
@@ -66,24 +77,44 @@ class LoginScreen(Screen):
 
             if stored_hash == salted_hash:
                 print('Login Successful')
-                break
+                self.manager.current = 'main'
+                # break
 
-            print('Your password is incorrect;Attempt:{}/5'.format(retry + 1))
+            else:
+                print('Your password is incorrect;Attempt:{}/5'.format(retry + 1))
+                retry = retry + 1
+                self.ids.inp_password.text = ''
+
+                if retry == 5:
+                    print('You have made too many attempts. Try again later!')
+                    print('Would you like to register a new account?')
+                    response = input('Yes or No: ')
+
+                    if response.lower == 'yes' or 'y':
+                        print('Starting register function...')
+                        retry = 0
+                        self.ids.inp_username.text = ''
+                        self.register_butt()
+
+                    elif response.lower == 'no' or 'n':
+                        print('Thank you for your time. Closing application...')
+                        sys.exit(1)
 
         else:
-            print('You have made too many attempts. Try again later!')
-            print('Would you like to register a new account?')
-            response = input('Yes or No: ')
-            if response.lower == 'yes' or 'y':
-                print('Starting register function...')
-                self.register()
-            else:
-                print('Thank you for your time. Closing application...')
-                sys.exit(1)
+            print('User does not exist')
+            self.ids.inp_username.text = ''
+            self.ids.inp_password.text = ''
 
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         # self.register()
+
+
+class RegisterPopup(Popup):
+    def register(self, uname, pwd):
+        reg = LoginScreen()
+        reg.register(uname, pwd)
+        self.dismiss()
 
 
 class MainScreen(Screen):
@@ -145,17 +176,55 @@ class MainScreen(Screen):
             print('Indexed item {} was deleted'.format(str(currentObj)))
             self.ids.rv.layout_manager.clear_selection()
 
-        def add_cart(self, cart_stat, cart_crm, cart_cpd, cart_frm, cart_fpd):
-            line = str(cart_stat) + ',' + str(cart_crm) + ',' + str(cart_cpd) + \
-                   ',' + str(cart_frm) + ',' + str(cart_fpd)
+        def add_cart(self, cart_type, cart_crm, cart_cpd, cart_frm, cart_fpd):
+            cart_id = len(self.ids.rv.data) + 1
+            comma_line = str(cart_id) + ',' + str(cart_type) + ',' + ',' + str(cart_crm) + ',' + \
+                str(cart_cpd) + ',' + str(cart_frm) + ',' + str(cart_fpd)
+
+            line = comma_line.replace(",", "              ")
+
+            self.ids.cart_type.text = ''
+            # self.ids.cart_stat.text = ''
+            self.ids.cart_crm.text = ''
+            self.ids.cart_cpd.text = ''
+            self.ids.cart_frm.text = ''
+            self.ids.cart_fpd.text = ''
 
             array2 = [{'text': line, 'is_selected': False}]
             self.ids.rv.data.extend(array2)
             print('New item added with value {}'.format(line))
+            self.show_add()
             self.ids.rv.refresh_from_data()
 
         def edit_cart(self):
             pass
+
+        def show_add(self):
+            global addState
+            global editState
+            addState = addState + 1
+            if addState % 2 == 0:
+                if editState % 2 == 0:
+                    self.show_edit()
+                slide_in = Animation(x=self.width)
+                slide_in.start(self.ids.AddBox)
+
+            else:
+                slide_out = Animation(x=-self.width)
+                slide_out.start(self.ids.AddBox)
+
+        def show_edit(self):
+            global editState
+            global addState
+            editState = editState + 1
+            if editState % 2 == 0:
+                if addState % 2 == 0:
+                    self.show_add()
+                slide_in = Animation(x=self.width)
+                slide_in.start(self.ids.EditBox)
+            else:
+                slide_out = Animation(x=-self.width)
+                slide_out.start(self.ids.EditBox)
 
         print("Connection succeeded...")
 
@@ -181,6 +250,7 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     def refresh_view_attrs(self, rv, index, data):
         """ Catch and handle the view changes """
         self.index = index
+
         return super(SelectableLabel, self).refresh_view_attrs(
             rv, index, data)
 
@@ -203,18 +273,10 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
             # print("selection removed for {0}".format(rv.data[index]))
 
 
-class AddEntryScreen(Screen):
-    def add_button(self, cart_stat, cart_crm, cart_cpd, cart_frm, cart_fpd):
-        main_screen = MainScreen()
-        main_screen.add_cart(cart_stat, cart_crm, cart_cpd, cart_frm, cart_fpd)
-
-
-class EditEntryScreen(Screen):
-    def edit_button(self):
+class SettingsScreen(Screen):
+    def set_sql_cred(self, sql_user, sql_pass, sql_port):
         pass
 
-
-class SettingsScreen(Screen):
     def set_time_5(self):
         print("You will be notified in 5 Minutes")
 
@@ -245,12 +307,13 @@ class ComputerCartMSApp(App):
         s.open()
 
     def build(self):
+        self.title = 'Computer Cart Management Software Alpha 4.0'
+        # self.icon = 'some cool logo I need to design'
+        self.icon = 'csslogocut small.png'
         sm = ScreenManager()
         sm.add_widget(LoginScreen(name='login'))
         sm.add_widget(MainScreen(name='main'))
         sm.add_widget(SettingsScreen(name='settings'))
-        sm.add_widget(AddEntryScreen(name='add'))
-        sm.add_widget(EditEntryScreen(name='edit'))
         sm.current = 'main'
         return sm
 
