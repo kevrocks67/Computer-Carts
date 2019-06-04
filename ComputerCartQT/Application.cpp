4c62cfa7be1a2c959ddf347912ae9a95a1cc4052
Kevin Diaz <kevrocks67@gmail.com>
2019-06-04 18:08:58 -0400

    Updated version number

--- ComputerCartQT/Application.cpp
+++ ComputerCartQT/Application.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     Application::setAttribute(Qt::AA_UseHighDpiPixmaps);
     Application::setOrganizationName("Columbia Secondary School");
     Application::setApplicationName("CCMS");
-    Application::setApplicationVersion("0.1");
+    Application::setApplicationVersion("0.2");
     Application::setFont(sansFont);
 
     Application app(argc, argv);
